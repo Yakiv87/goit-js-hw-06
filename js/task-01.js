61b67f9475e0e65650b48cc18c3334ffc3cf1bcb
Yakiv87 <130331039+Yakiv87@users.noreply.github.com>
2023-08-26 19:03:04 -0400

    upgrade

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,11 @@
-const list = document.querySelector("#categories")
-console.dir(list);
+const itemsEl = document.querySelectorAll("#categories .item");
+console.log(`Number of categories: ${itemsEl.length}`);
+
+
+itemsEl.forEach(item => {
+  const categoryName = item.querySelector('h2').textContent;
+  const categoryCount = item.querySelectorAll('li').length;
+
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: ${categoryCount}`);
+});
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,4 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientsEl = document.querySelector('.js-item');
