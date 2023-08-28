0fd7c9cc4fc292d4b3c5c7a208ba43c0c991de02
Yakiv87 <130331039+Yakiv87@users.noreply.github.com>
2023-08-27 23:36:10 -0400

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,4 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-const ingredientsEl = document.querySelector('.js-item');
+const ingredientsList = document.querySelector("#ingredients");
+
+const ingredientsItems = ingredients.map((ingredient) => {
+  const li = document.createElement("li");
+  li.textContent = ingredient;
+  li.classList.add("item");
+  return li;
+});
+console.log(ingredientsItems);
+ingredientsList.append(...ingredientsItems);
