88fec0594a2e9d4aabc3c289f84d569ec93a9556
Yakiv87 <130331039+Yakiv87@users.noreply.github.com>
2023-09-04 00:24:42 -0400

    Update task-07.js

---
+++ js/task-07.js
@@ -0,0 +1,9 @@
+
+const fontSizeControl = document.querySelector('#font-size-control');
+const text = document.querySelector('#text');
+
+fontSizeControl.addEventListener('input', () => {
+  const size = fontSizeControl.value;
+  text.style.fontSize = `${size}px`; 
+});
+console.log(fontSizeControl)
