a96b5156dc2d87178bc1b2ad53309ca3dfb6ca36
Yakiv87 <130331039+Yakiv87@users.noreply.github.com>
2023-09-05 00:49:48 -0400

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
-
+const feedbackForm = document.querySelector(".login-form");
+feedbackForm.addEventListener("submit", handelSubmit);
+function handelSubmit(event) {
+    event.preventDefault();
+    const email = feedbackForm.elements.email.value;
+    const password = feedbackForm.elements.password.value;
+  
+    if (email === '' || password === '') {
+      alert('Всі поля повинні бути заповнені');
+    } else {
+      const formData = {
+        email: email,
+        password: password
+      };
+  
+      console.log(formData);
+  
+      form.reset();
+};
+}
